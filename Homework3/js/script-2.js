e0597d33df251a583e6d738b0bec892b5d283b12
Nikita <162756288+McQueenMe@users.noreply.github.com>
2024-05-24 23:38:41 +0300

    work - 2

---
+++ Homework3/js/script-2.js
@@ -0,0 +1,5 @@
+let name = prompt("Input your name:");
+let surname = prompt("Input your surname:");
+let age = prompt("Input your age:");
+
+console.log(`first: ${surname}, second: ${age}, third: ${name}`);
